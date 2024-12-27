6685c8971b0e382897446df584eacac2ee639350
Ramsiddharth21 <siddharthrampoosarla@gmail.com>
2024-12-27 10:32:54 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+
+int removeElement(int arr[], int n, int key) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != key) {
+            arr[count++] = arr[i];
+        }
+    }
+    return count;
+}
+
+
+int main() {
+    int arr[] = {3, 2, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key;
+
+
+    printf("Enter the key to remove: ");
+    scanf("%d", &key);
+
+
+    int newLength = removeElement(arr, n, key);
+
+
+    printf("Number of elements not equal to %d: %d\n", key, newLength);
+    printf("Updated array: ");
+    for (int i = 0; i < newLength; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+
+    return 0;
+}
---
+++ 3.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+
+void bestTimeToSellCommodity(int prices[], int n, int *buyDay, int *sellDay) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+    *buyDay = 0;
+    *sellDay = 0;
+
+
+    for (int i = 1; i < n; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+            *buyDay = i;  // Update buy day
+        }
+
+
+        int profit = prices[i] - minPrice;
+        if (profit > maxProfit) {
+            maxProfit = profit;
+            *sellDay = i;  // Update sell day
+        }
+    }
+}
+
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int n = sizeof(prices) / sizeof(prices[0]);
+    int buyDay, sellDay;
+
+
+    bestTimeToSellCommodity(prices, n, &buyDay, &sellDay);
+
+
+    printf("Best day to buy: %d\n", buyDay + 1);  // 1-based index
+    printf("Best day to sell: %d\n", sellDay + 1);  // 1-based index
+
+
+    return 0;
+}
